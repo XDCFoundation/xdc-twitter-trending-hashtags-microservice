6cb7c53f2c6af848c10520da2b4b0ceb185f3b3f
Deepak Tomar <deepakt@leewayhertz.com>
2022-02-10 16:58:33 +0530

    getTrendingHashTagsFromDB API created

--- app/modules/trendingHashtag/index.js
+++ app/modules/trendingHashtag/index.js
@@ -2,11 +2,24 @@
  * Created by Developer on 13/05/21.
  */
 
- const BlManager = require("./manager");
- class Manger {
+import BlManager  from "./manager";
+import {apiSuccessMessage,httpConstants} from "../../common/constants";
+import Utils from "../../utils"
+
+ class TrendingController {
      async getTrendingHashtags() {
          return await new BlManager().getTrendingHashtags();
      }
+
+     async getTrendingHashtagsFromDB(request,response) {
+         Utils.lhtLog("TrendingController:getTrendingHashtagsFromDB", "", "", '', httpConstants.LOG_LEVEL_TYPE.INFO);
+         let [error, latestAccountsResponse] = await Utils.parseResponse(new BlManager().getTrendingHashtagsFromDB(request.query));
+         if (error) {
+             Utils.lhtLog("TrendingController:getTrendingHashtagsFromDB", "getTrendingHashtagsFromDB err", error, "", "ERROR")
+             return Utils.handleError([error], request, response);
+         }
+         return Utils.response(response, latestAccountsResponse, apiSuccessMessage.FETCH_SUCCESS, httpConstants.RESPONSE_STATUS.SUCCESS, httpConstants.RESPONSE_CODES.OK);
+     }
  }
- module.exports = Manger;
+ module.exports = TrendingController;
  
